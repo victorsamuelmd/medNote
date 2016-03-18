1c423e36adcd1c32d0c2dd7b8abef0f0f678a7e9
Victor Samuel Mosquera Artamonov <victorsamuelmd@gmail.com>
2016-03-17 19:11:45 -0500

    Borre unas lineas innecesarias

--- static/app.js
+++ static/app.js
@@ -12,28 +12,6 @@
         talla = document.querySelector('#talla').value;
         document.querySelector('#imc').value = imc(peso, talla);
     }
-
-
-    verFormula = function(){
-        var data = new FormData(document.getElementsByTagName('form')[0]);
-        var reg = new XMLHttpRequest();
-        reg.onload = function(){
-                var file = window.URL.createObjectURL(reg.response);
-                var a = document.createElement("a");
-                a.href = file;      
-                window.open(file);
-                console.log('hola');
-        }
-        reg.open("POST", '/formula');
-        reg.send({
-            'nombre': '' + data.get('pnombre') + data.get('snombre') + data.get('papellido'),
-            'id': data.get('cedula'),
-            'eps': 'Asmetsalud',
-            'centro-salud': 'Arboleda',
-            'receta': data.get('conducta')
-        }); 
-    }
     
     document.querySelector('#talla').addEventListener('blur', showIMC);
-    document.querySelector('#formula-pdf').addEventListener('click', verFormula);
 })();
