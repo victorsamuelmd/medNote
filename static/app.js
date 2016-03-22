879cf10f7f19300b821fa89fd79081d81665d872
Victor Samuel Mosquera Artamonov <victorsamuelmd@gmail.com>
2016-03-22 15:59:19 -0500

    Resolviendo algunos errores

--- static/app.js
+++ static/app.js
@@ -4,14 +4,14 @@
 
     imc = function(peso, talla){
         return peso/(talla*talla);
-    }
+    };
 
     showIMC = function(){
         var peso, talla;
         peso = document.querySelector('#peso').value;
         talla = document.querySelector('#talla').value;
         document.querySelector('#imc').value = imc(peso, talla);
-    }
+    };
     
     document.querySelector('#talla').addEventListener('blur', showIMC);
 })();
--- static/remision.js
+++ static/remision.js
@@ -1,52 +1,58 @@
-f_data = new FormData(document.querySelector('#remisionForm'));
 
 function RFC3339(d){
     function pad(n){
-        return n<10 ? '0'+n : n
+        return n < 10 ? '0' + n : n;
     }
-    return d.getUTCFullYear()+'-'
-        + pad(d.getMonth()+1)+'-'
-        + pad(d.getDate())+'T'
-        + pad(d.getHours())+':'
-        + pad(d.getMinutes())+':'
-        + pad(d.getSeconds())+'-05:00'
+    return d.getUTCFullYear() + '-' +
+        pad(d.getMonth()+1) + '-' +
+        pad(d.getDate())+'T' + 
+        pad(d.getHours())+':' + 
+        pad(d.getMinutes())+':' + 
+        pad(d.getSeconds())+'-05:00';
 }
 
 fecha = new Date();
 
 
 var hacerRemision = function(){
-    var remision = {
-        'medico': {
-            'primer-nombre': 'Victor',
-            'segundo-nombre': 'Samuel',
-            'primer-apellido': 'Mosquera',
-            'segundo-apellido': 'A.',
-            'identificacion': '1087998004',
-            'tipo-identificacion': 'CC'
-        },
-        'paciente': {
-            'primer-nombre': f_data.get('pnombre'),
-            'segundo-nombre': f_data.get('snombre'),
-            'primer-apellido': f_data.get('papellido'),
-            'segundo-apellido': f_data.get('sapellido'),
-            'fecha-nacimiento': function(){
-                    var a = new Date(1987, 07, 16); return RFC3339(a)}()
-        },
-        'receptor': f_data.get('eps'),
-        'servicio': f_data.get('servicio'),
-        'fecha': RFC3339(fecha),
-        'contenido': f_data.get('contenido')
+    var remision = function(){
 
-    }
+        f_data = new FormData(document.querySelector('#remisionForm'));
+        var data = {
+            'medico': {
+                'primer-nombre': 'Victor',
+                'segundo-nombre': 'Samuel',
+                'primer-apellido': 'Mosquera',
+                'segundo-apellido': 'A.',
+                'identificacion': '1087998004',
+                'tipo-identificacion': 'CC'
+            },
+            'paciente': {
+                'primer-nombre': f_data.get('pnombre'),
+                'segundo-nombre': f_data.get('snombre'),
+                'primer-apellido': f_data.get('papellido'),
+                'segundo-apellido': f_data.get('sapellido'),
+                "identificacion": f_data.get("cedula"),
+                'fecha-nacimiento': function(){
+                    var a = new Date(2009, 8, 19); return RFC3339(a);}()
+            },
+            'receptor': f_data.get('eps'),
+            'servicio': f_data.get('servicio'),
+            'fecha': RFC3339(fecha),
+            'contenido': f_data.get('contenido')
+        };
+        console.log(data);
+        return data;
+    };
     rem = new XMLHttpRequest();
     rem.responseType = "arrayBuffer";
     rem.onload = function(data){
         var pdf = "data:application/pdf;base64," + rem.response;
         document.querySelector("#some").src = pdf;
-    }
+    };
     rem.open("post", "/remision");
-    rem.send(JSON.stringify(remision));
-}
+    rem.send(JSON.stringify(remision()));
+};
 
-document.querySelector('#remitir').addEventListener('mousedown', hacerRemision);
+document.querySelector('#remitir').addEventListener('mousedown', 
+        function(){hacerRemision();});
